1b87281cafd2ac1fc115da72c0ec192aa8d39a36
Nickname <1013238728@qq.com>
2025-03-03 00:40:24 +0800

    初始化项目模板

---
+++ sql/interview.sql
@@ -0,0 +1,192 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : yixin-outer.mysql.rds.aliyuncs.com_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80036 (8.0.36)
+ Source Host           : yixin-outer.mysql.rds.aliyuncs.com:3306
+ Source Schema         : interview
+
+ Target Server Type    : MySQL
+ Target Server Version : 80036 (8.0.36)
+ File Encoding         : 65001
+
+ Date: 02/03/2025 23:53:32
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for post
+-- ----------------------------
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `title` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '标题',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '内容',
+  `tags` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '标签列表（json 数组）',
+  `thumbNum` int NOT NULL DEFAULT 0 COMMENT '点赞数',
+  `favourNum` int NOT NULL DEFAULT 0 COMMENT '收藏数',
+  `userId` bigint NOT NULL COMMENT '创建用户 id',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint NOT NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_userId`(`userId` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '帖子' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of post
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for post_favour
+-- ----------------------------
+DROP TABLE IF EXISTS `post_favour`;
+CREATE TABLE `post_favour`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `postId` bigint NOT NULL COMMENT '帖子 id',
+  `userId` bigint NOT NULL COMMENT '创建用户 id',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_postId`(`postId` ASC) USING BTREE,
+  INDEX `idx_userId`(`userId` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '帖子收藏' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of post_favour
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for post_thumb
+-- ----------------------------
+DROP TABLE IF EXISTS `post_thumb`;
+CREATE TABLE `post_thumb`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `postId` bigint NOT NULL COMMENT '帖子 id',
+  `userId` bigint NOT NULL COMMENT '创建用户 id',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_postId`(`postId` ASC) USING BTREE,
+  INDEX `idx_userId`(`userId` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '帖子点赞' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of post_thumb
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for question
+-- ----------------------------
+DROP TABLE IF EXISTS `question`;
+CREATE TABLE `question`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `title` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '标题',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '内容',
+  `tags` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '标签列表（json 数组）',
+  `answer` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '推荐答案',
+  `userId` bigint NOT NULL COMMENT '创建用户 id',
+  `reviewStatus` int NOT NULL DEFAULT 0 COMMENT '状态：0-待审核, 1-通过, 2-拒绝',
+  `reviewMessage` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '审核信息',
+  `reviewerId` bigint NULL DEFAULT NULL COMMENT '审核人 id',
+  `reviewTime` datetime NULL DEFAULT NULL COMMENT '审核时间',
+  `viewNum` int NOT NULL DEFAULT 0 COMMENT '浏览量',
+  `thumbNum` int NOT NULL DEFAULT 0 COMMENT '点赞数',
+  `favourNum` int NOT NULL DEFAULT 0 COMMENT '收藏数',
+  `priority` int NOT NULL DEFAULT 0 COMMENT '优先级',
+  `source` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '题目来源',
+  `needVip` tinyint NOT NULL DEFAULT 0 COMMENT '仅会员可见（1 表示仅会员可见）',
+  `editTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '编辑时间',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint NOT NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_title`(`title` ASC) USING BTREE,
+  INDEX `idx_userId`(`userId` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '题目' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of question
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for question_bank
+-- ----------------------------
+DROP TABLE IF EXISTS `question_bank`;
+CREATE TABLE `question_bank`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `title` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '标题',
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '描述',
+  `picture` varchar(2048) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '图片',
+  `userId` bigint NOT NULL COMMENT '创建用户 id',
+  `reviewStatus` int NOT NULL DEFAULT 0 COMMENT '状态：0-待审核, 1-通过, 2-拒绝',
+  `reviewMessage` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '审核信息',
+  `reviewerId` bigint NULL DEFAULT NULL COMMENT '审核人 id',
+  `reviewTime` datetime NULL DEFAULT NULL COMMENT '审核时间',
+  `priority` int NOT NULL DEFAULT 0 COMMENT '优先级',
+  `viewNum` int NOT NULL DEFAULT 0 COMMENT '浏览量',
+  `editTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '编辑时间',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint NOT NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_title`(`title` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '题库' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of question_bank
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for question_bank_question
+-- ----------------------------
+DROP TABLE IF EXISTS `question_bank_question`;
+CREATE TABLE `question_bank_question`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `questionBankId` bigint NOT NULL COMMENT '题库 id',
+  `questionId` bigint NOT NULL COMMENT '题目 id',
+  `userId` bigint NOT NULL COMMENT '创建用户 id',
+  `questionOrder` int NOT NULL DEFAULT 0 COMMENT '题目顺序（题号）',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `questionBankId`(`questionBankId` ASC, `questionId` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '题库题目' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of question_bank_question
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `userAccount` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '账号',
+  `userPassword` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '密码',
+  `unionId` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '微信开放平台id',
+  `mpOpenId` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '公众号openId',
+  `userName` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户昵称',
+  `userAvatar` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户头像',
+  `userProfile` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户简介',
+  `userRole` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'user' COMMENT '用户角色：user/admin/vip/ban',
+  `vipExpireTime` datetime NULL DEFAULT NULL COMMENT '会员过期时间',
+  `vipCode` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '会员兑换码',
+  `vipNumber` bigint NULL DEFAULT NULL COMMENT '会员编号',
+  `editTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '编辑时间',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint NOT NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_unionId`(`unionId` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ src/main/java/com/yixin/interview/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.yixin.interview.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yixin.interview.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
